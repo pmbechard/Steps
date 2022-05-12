e3925018e2569000a4dc4d04f1ad90aa7b4be347
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-12 08:53:25 +0800

    Adds more advanced styling to list items, header, and footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+import logo from './img/steps_logo.png';
+import gitHubLogo from './img/github.png';
+
+const header = document.querySelector('header');
+const logoImg = new Image();
+logoImg.src = logo;
+header.insertBefore(logoImg, header.firstChild);
+header.firstChild.style.width = '100px';
+
+const footer = document.querySelector('footer');
+const gitHubImg = new Image();
+gitHubImg.src = gitHubLogo;
+footer.insertBefore(gitHubImg, footer.firstChild);
+gitHubImg.classList.add('footer-link')
+gitHubImg.onclick = () => open('https://github.com/pmbechard/', '_blank');
